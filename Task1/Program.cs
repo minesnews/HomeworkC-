2703b182b28a03bc8000db0bba26811ea7ebeeef
Nikolay Chelpanov <nikch3008@mail.ru>
2024-02-18 16:10:33 +0700

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿void OutputOfNumbers(int m, int n)
+{
+    int result = m;
+    if(result == n)
+    {
+        System.Console.Write(result);
+        return;
+    }
+    System.Console.Write($"{result} ");
+    OutputOfNumbers(m+1, n);
+}
+
+int m = 10;
+int n = 20;
+
+OutputOfNumbers(m, n);
