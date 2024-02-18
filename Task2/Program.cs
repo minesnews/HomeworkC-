3cc20014bc442d27a6117c3aefe523af13bec00d
Nikolay Chelpanov <nikch3008@mail.ru>
2024-02-18 16:10:49 +0700

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int FunctionAckerman(int m, int n)
+{    
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionAckerman(m - 1, 1);
+    else if (m > 0 && n > 0) return FunctionAckerman(m-1, FunctionAckerman(m, n-1));
+    return FunctionAckerman(m, n);
+}
+
+int m = 3;
+int n = 5;
+int result = FunctionAckerman(m, n);
+System.Console.WriteLine(result);
+
