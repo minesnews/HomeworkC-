5e03c3834506186789ca8b6aa5a952eb14fb61cc
Nikolay Chelpanov <nikch3008@mail.ru>
2024-02-18 16:10:57 +0700

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintReverseArray(int[] array, int length)
+{
+    
+    if(length == 0)
+    {
+        return;
+    }
+    System.Console.Write($"{array[length - 1]} ");
+    PrintReverseArray(array, length - 1);
+}
+
+int[] array = { 1, 2, 3, 4, 5, -1, 10 };
+int length = array.Length;
+PrintReverseArray(array, length);
